04012c8eadc7d0cf219ae2356c2d2e6cb7657db6
Alan Garny <agarny@hellix.com>
2020-06-04 18:25:40 +1200

    Generate a usage message if we don't provide both a URL and a config file (#1).
    Note: at this stage, we only make use of the URL file.

--- opencor.py
+++ opencor.py
@@ -1,10 +1,11 @@
 import json
+import sys
 
 import opencor as oc
 
-if __name__ == "__main__":
-    s = oc.open_simulation('https://models.physiomeproject.org/workspace/5f5/rawfile'
-                           '/125f548ce204c1d815298d2c8c1d9b774d89e3a7/mcintyre_richardson_grill_model_2001.sedml')
+
+def main(url, config):
+    s = oc.open_simulation(url)
 
     s.run()
 
@@ -16,3 +17,44 @@ if __name__ == "__main__":
         voi.uri(): voi.values().tolist(),
         v_m.uri(): v_m.values().tolist()
     }, indent=2))
+
+
+def usage():
+    print('Usage: docker run opencor <url> <config>')
+    print('  where <url> is the URL to a CellML / SED-ML file')
+    print('e.g. https://models.physiomeproject.org/workspace/5f5/rawfile/125f548ce204c1d815298d2c8c1d9b774d89e3a7'
+          '/mcintyre_richardson_grill_model_2001.sedml')
+    print('  where <config> is the filename of a configuration (JSON) file.')
+    print('    e.g. config.json')
+    print('    {')
+    print('      "simulation": {')
+    print('        "Starting point": 0,')
+    print('        "Ending point": 100,')
+    print('        "Point interval": 0.1')
+    print('      },')
+    print('      "parameters": {')
+    print('        "fast_sodium_channel/g_Naf": 3,')
+    print('        "membrane/stim_amplitude": 0.05,')
+    print('        "membrane/stim_duration": 0.5')
+    print('      },')
+    print('      "output": [')
+    print('        "membrane/V_m",')
+    print('        "membrane/V_m/prime"')
+    print('      ]')
+    print('    }')
+
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    args.pop(0)
+
+    try:
+        url = args.pop(0)
+        config = args.pop(0)
+    except:
+        usage()
+
+        sys.exit(1)
+
+    main(url, config)
