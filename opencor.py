efc3089c5bc0e6edc9627056a8c5444369edc95d
Alan Garny <agarny@hellix.com>
2020-06-05 16:08:51 +1200

    Some minor cleaning up.

--- opencor.py
+++ opencor.py
@@ -112,26 +112,7 @@ def usage():
     print('Usage: docker run opencor <url>')
     print('   or: docker run -i opencor <url> < <config>')
     print('  where <url> is the URL of a CellML / SED-ML file')
-    print('    e.g. https://models.physiomeproject.org/workspace/5f5/rawfile/125f548ce204c1d815298d2c8c1d9b774d89e3a7'
-          '/mcintyre_richardson_grill_model_2001.sedml')
-    print('  where <config> is the filename of a configuration (JSON) file.')
-    print('    e.g. config.json')
-    print('    {')
-    print('      "simulation": {')
-    print('        "Starting point": 0,')
-    print('        "Ending point": 100,')
-    print('        "Point interval": 0.1')
-    print('      },')
-    print('      "parameters": {')
-    print('        "fast_sodium_channel/g_Naf": 3,')
-    print('        "membrane/stim_amplitude": 0.05,')
-    print('        "membrane/stim_duration": 0.5')
-    print('      },')
-    print('      "output": [')
-    print('        "membrane/V_m",')
-    print('        "membrane/V_m/prime"')
-    print('      ]')
-    print('    }')
+    print('  where <config> is the filename of a configuration file.')
 
 
 if __name__ == "__main__":
